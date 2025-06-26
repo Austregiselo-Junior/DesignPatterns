ce69b97a5776ad9616a520b5c6844d80398ecde9
Austregiselo Junior <austregiselosvj@gmail.com>
2025-06-26 14:59:53 -0300

    Dependency-Injection: Create services

---
+++ Dependency-Injection/Dependency Injection/DataBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependency_Injection
+{
+    internal class DataBaseService : IDataService
+    {
+        public void Save(string message)
+        {
+            Console.WriteLine("Save the message into the data base");
+        }
+    }
+}
---
+++ Dependency-Injection/Dependency Injection/FileService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependency_Injection
+{
+    internal class FileService : IFileService
+    {
+        public void SaveFile(string message)
+        {
+            Console.WriteLine("Save the message into the file");
+        }
+    }
+}
