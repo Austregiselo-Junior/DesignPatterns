4ca67927441f5140baaeb133733b9ff2b0de7575
Austregiselo Junior <austregiselosvj@gmail.com>
2025-06-26 15:05:11 -0300

    Dependency-Injection: Create message class

---
+++ Dependency-Injection/Dependency Injection/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace Dependency_Injection
+{
+    internal class Message
+    {
+        private readonly IDataService _dataService;
+        private readonly IFileService _fileService;
+
+        public Message(IDataService dataService, IFileService fileService)
+        {
+            _dataService = dataService;
+            _fileService = fileService;
+        }
+
+        private void SaveMessageDataBase(string message)
+        {
+            _dataService.Save(message);
+        }
+
+        private void SaveMessageFile(string message)
+        {
+            _fileService.SaveFile(message);
+        }
+    }
+}
