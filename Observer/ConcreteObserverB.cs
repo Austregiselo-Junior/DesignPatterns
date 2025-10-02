77d0cd2a431f3f6cd7c3cd4f5c85b5043f2ab00f
Austregiselo Junior <austregiselosvj@gmail.com>
2025-10-02 08:03:09 -0300

    Observer: Fix update logic fom ConcreteObserverB.

--- Observer/ConcreteObserverB.cs
+++ Observer/ConcreteObserverB.cs
@@ -6,7 +6,7 @@ namespace Observer
     {
         public void Update(ISubject subject)
         {
-            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            if ((subject as Subject).State == 0 || (subject as Subject).State <= 2)
             {
                 Console.WriteLine("ConcreteObserverB (Observador): Reagindo ao evento");
             }
